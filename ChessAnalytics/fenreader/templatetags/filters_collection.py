c7169d5bbaa5e43c06cfa83fb5d7d0b460ea60b6
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2023-08-10 20:03:38 +0300

    filter for move number

---
+++ ChessAnalytics/fenreader/templatetags/filters_collection.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def to_move_number(value, is_white_to_move):
+    if is_white_to_move is True:
+        if value % 2 == 0 or value == 0:
+            return ""
+        else:
+            return f'{value // 2}. '
+    else:
+        if value == 1:
+            return "1 ... "
+        elif value % 2 == 0:
+            return f'  {value // 2 + 1}. '
+        else:
+            return ''
