5909e814ffbcf35b391b3cf53fd4b2bb80a1869c
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2023-09-08 14:41:15 +0300

    JS refactoring and readability

--- static/scripts/board-drag-script.js
+++ static/scripts/board-drag-script.js
@@ -13,44 +13,18 @@ async function drop(ev) {
     var pieceID = ev.dataTransfer.getData("text");
     var comes_from = pieceID.slice(-2);
     var goes_to = ev.target.id.slice(-2)
-    const headersForMakingAMove = {
+    const headersForMakingAMove = createHeaderForMakingAMove(csrftoken, comes_from, goes_to)
 
-        method: "POST",
-        headers: {'X-CSRFToken': csrftoken, 'Content-Type': 'application/json'},
-        body: JSON.stringify({comes_from, goes_to}),
-        mode: 'same-origin'
-    }
-    console.log(comes_from)
-    console.log(goes_to)
-    console.log(JSON.stringify(headersForMakingAMove, null, 2));
     let res =  await fetch(BASE_URL, headersForMakingAMove)
     let unparsedData = await res.json()
     var data = JSON.parse(unparsedData);
     console.log(data)
     if (data.is_legal){
         if (data.is_promotion != true){
-            let newSquare = document.getElementById(goes_to)
-            newSquare.innerHTML = ""
-            let pieceImage = document.getElementById(pieceID)
-
-            newSquare.appendChild(pieceImage)
+            performNonPromotionMove(goes_to, pieceID)
         }
     }else if (data.is_promotion == true) {
-
-        const promotionChoiceContainer = document.createElement('div')
-
-        const knightChoice = createPromotionChoiceDiv('knight', 'black', comes_from, goes_to)
-        const bishopChoice = createPromotionChoiceDiv('bishop', 'black', comes_from, goes_to)
-        const rookChoice = createPromotionChoiceDiv('rook', 'black', comes_from, goes_to)
-        const queenChoice = createPromotionChoiceDiv('queen', 'black', comes_from, goes_to)
-
-        promotionChoiceContainer.appendChild(knightChoice)
-        promotionChoiceContainer.appendChild(bishopChoice)
-        promotionChoiceContainer.appendChild(rookChoice)
-        promotionChoiceContainer.appendChild(queenChoice)
-
-        const page = document.getElementsByClassName('wrapper')[0]
-        page.appendChild(promotionChoiceContainer)
+        createPromotionChoiceContainer(comes_from, goes_to)
     }
 }
 
@@ -67,4 +41,38 @@ function createPromotionChoiceDiv(piece_type, piece_color, comes_from, goes_to){
 function sendPromotionChoice(event){
     console.log(this.id)
 
+}
+
+function createHeaderForMakingAMove(csrftoken, comes_from, goes_to, promotes_to = null){
+    return {
+        method: "POST",
+        headers: {'X-CSRFToken': csrftoken, 'Content-Type': 'application/json'},
+        body: JSON.stringify({comes_from, goes_to, promotes_to}),
+        mode: 'same-origin'
+    }
+}
+
+function performNonPromotionMove(goes_to, pieceID) {
+    let newSquare = document.getElementById(goes_to)
+    newSquare.innerHTML = ""
+    let pieceImage = document.getElementById(pieceID)
+
+    newSquare.appendChild(pieceImage)
+}
+
+function createPromotionChoiceContainer(comes_from, goes_to) {
+            const promotionChoiceContainer = document.createElement('div')
+
+        const knightChoice = createPromotionChoiceDiv('knight', 'black', comes_from, goes_to)
+        const bishopChoice = createPromotionChoiceDiv('bishop', 'black', comes_from, goes_to)
+        const rookChoice = createPromotionChoiceDiv('rook', 'black', comes_from, goes_to)
+        const queenChoice = createPromotionChoiceDiv('queen', 'black', comes_from, goes_to)
+
+        promotionChoiceContainer.appendChild(knightChoice)
+        promotionChoiceContainer.appendChild(bishopChoice)
+        promotionChoiceContainer.appendChild(rookChoice)
+        promotionChoiceContainer.appendChild(queenChoice)
+
+        const page = document.getElementsByClassName('wrapper')[0]
+        page.appendChild(promotionChoiceContainer)
 }
